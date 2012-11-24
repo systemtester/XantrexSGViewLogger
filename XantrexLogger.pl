af1183b73c61f49918f5b3a882c4b67dc88a61cb
System Tester <systemtester@internode.on.net>
2012-11-25 09:02:32 +1000

    modified:   XantrexLogger.pl 	Extra Commenting and context added

--- XantrexLogger.pl
+++ XantrexLogger.pl
@@ -4,23 +4,25 @@ use strict;
 use Text::CSV;
 use Device::SerialPort;
 
+# This scripts creates a log file very similar to the Windows based SG-View software
+# A lot of what is logged by that software, and this script, is not used by the PVOutput
+# Integration Service (yet)
 # Setup vars for log file creation.
 # If using the PVOutput Integration Service ensure the file naming structure
 # defined in pvoutput.ini is the same as the code below
 # The filename compiled below uses the following file name
-# SolarDailyLog_MM-dd-yyyy
+# solarDataLog MM-dd-yyyy
 my $serial_port ="/dev/ttyS0";  #dmesg | grep tty to find this
 my $serial_lock = "/tmp/ttyS0.lock";
 my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime;
 my $Time = sprintf('%02d', $hour).':'.sprintf('%02d', $min).':'.sprintf('%02d', $sec);
 $mon++;
 $year += 1900;
-my $Date = $mon."-".$mday."-".$year;
+my $Date = $mon."-".$mday."-".$year; #Important that the month/day ordering follows the logfile month/day ordering.
 my $logdir="/var/log/pvoutput"; #All Xantrex Logs will be stored here.  Ensure pvoutput.ini is configured to look here.
 my $logfile = "$logdir/solarDataLog ".sprintf('%02d',$mon)."-".sprintf('%02d',$mday)."-".sprintf('%02d',$year);
 my $logdata;
-#my $csv = Text::CSV->new ({binary => 1}) or die "Cannot use CSV: ".Text::CSV->error_diag ();
-my $csv = Text::CSV->new ({ binary => 1, sep_char => "\t"});
+my $csv = Text::CSV->new ({ binary => 1, sep_char => "\t"}) or die "Cannot use CSV: ".Text::CSV->error_diag (); #Default sep_char is COMMA
 my ($VDC,$DCAmps,$MPPT,$DCPwr,$ACPwr,$Eff,$VAC,$ACKWh, $ACWh,$HSTemp,$Freq, $STATUS);
 
 # Wait until unlocked
@@ -59,7 +61,7 @@ if ($DCAmps && $DCAmps > 0) #Don't Log if Inverter is not getting the juice
 	(my $ACPwrCharCount, $ACPwr) = $serial_port->read(255); 
 
 	# The SG-View software logs a calculated Efficiency value
-	# The Inverter does not store or calculate this
+	# The Inverter does not store or calculate this so we're calculating it now
 	my ($num, $mantissa);
 	if ($ACPwr && $ACPwr > 0)
 	{
@@ -76,11 +78,10 @@ if ($DCAmps && $DCAmps > 0) #Don't Log if Inverter is not getting the juice
 	$serial_port->write("VOUT?\r");
 	(my $VACCharCount, $VAC) = $serial_port->read(255);
 
-	$serial_port->write("KWHTODAY?\r"); # Inverters energy production today (KWh)
+	$serial_port->write("KWHTODAY?\r"); # Inverter energy production today (KWh)
 	(my $ACKWhCharCount, $ACKWh) = $serial_port->read(255);
 
-	$ACWh = 1000 * $ACKWh;
-	#$ACWh = chop($ACWh); #Convert to Wh and drop the decimal
+	$ACWh = 1000 * $ACKWh; #Needs conversion to send to PVOutput
 
 	$serial_port->write("MEASTEMP?\r"); #Response is 'C:[a] F:[b]' [a] = 0-125.0 [b] = 32.0-257.0
 	(my $HCTempCharCount, $HSTemp) = $serial_port->read(255);
@@ -90,7 +91,7 @@ if ($DCAmps && $DCAmps > 0) #Don't Log if Inverter is not getting the juice
 	(my $FreqCharCount, $Freq) = $serial_port->read(255);
 	($num, $mantissa) = split ('\.', $Freq);
 	$mantissa = substr($mantissa, 0, 1);
-	if ($mantissa == 0) {
+	if ($mantissa == 0) { #Following logic used in SG View here.  .0 is dropped and the whole number remains.
 		$Freq = $num;
 	}
 	else {
@@ -108,7 +109,7 @@ if ($DCAmps && $DCAmps > 0) #Don't Log if Inverter is not getting the juice
 	}
 	
 	my @data = ($Date,$Time,$VDC,$DCAmps,$MPPT,$DCPwr,$ACPwr,$Eff,$VAC,$ACWh,$HSTemp,$Freq, $STATUS);
-	foreach my $data (@data) #Remove trailing CR
+	foreach my $data (@data) #Remove trailing CR from each Inverter-supplied value.  csv->combine needs this
 	{
 		$data =~ s/^\s+//;
 		$data =~ s/\s+$//;
@@ -133,4 +134,4 @@ close LOGFILE;
 }
 $serial_port->close || warn "close failed";
 undef $serial_port;
-exit 0; 
+exit 0;
